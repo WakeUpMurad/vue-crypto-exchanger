ade579463d28439a3ab507e40c8302da60b9e007
WakeUpMurad <murad.losangeles@gmail.com>
2024-02-28 20:43:39 +0300

    make crypto exchanger

---
+++ src/components/Favourite.vue
@@ -0,0 +1,47 @@
+<template>
+    <h2>Варианты в избранном</h2>
+    <div @click="getFromFavs(index)" v-for="(fav, index) in favs" :key="index">
+        <span>{{ fav.from }}</span>
+        <span>{{ fav.to }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        favs: {
+            type: Array,
+            required: true
+        },
+        getFromFavs: {
+            type: Function,
+            required: true
+        },
+    }
+}
+</script>
+
+<style scoped>
+h2 {
+    color: #fff;
+}
+
+div {
+    display: flex;
+    justify-content: space-between;
+    width: 500px;
+    margin: 10px auto;
+    transition: all 500ms ease;
+}
+
+div:hover {
+    opacity: 0.7;
+    transform: scale(1.1);
+    cursor: pointer;
+}
+
+span {
+    padding: 10px 15px;
+    background: #8c00ff;
+    color: #fff;
+}</style>
---
+++ src/components/Input.vue
@@ -0,0 +1,51 @@
+<template>
+  <input min="0" type="number" @input="changeAmound($event.target.value)"><br>
+  <button @click="convert()">Конвертировать</button>
+  <button @click="favourite()" className="fuv-button">В избранное</button>
+</template>
+
+<script>
+export default {
+  props: {
+    changeAmound: {
+      type: Function,
+      required: true
+    },
+    convert: {
+      type: Function,
+      required: true
+    },
+    favourite: {
+      type: Function,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+input {
+  width: 500px;
+  position: relative;
+  top: -50px;
+  font-size: 1.25rem;
+  border-radius: 3px;
+  border: 4px solid #1a032d;
+  padding: 10px 15px;
+  background: #fafafa;
+}
+
+button {
+  position: relative;
+  top: -20px;
+  padding: 10px 15px;
+  color: #fff;
+  text-transform: uppercase;
+  cursor: pointer;
+  background: #1a032d;
+}
+
+button.fuv-button {
+  margin-left: 20px;
+}
+</style>
